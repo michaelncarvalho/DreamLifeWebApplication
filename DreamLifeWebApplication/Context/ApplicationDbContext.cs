8bc8abf3de32bca3935acb14c56a1c5b795e6b97
michaelncarvalho <45688204+michaelncarvalho@users.noreply.github.com>
2019-04-15 16:45:52 -0300

    Configuração npgsql e EF
    Configurações do banco de dados para possibilitar a conexão entre o npgsql e o EF.

---
+++ DreamLifeWebApplication/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DreamLifeWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DreamLifeWebApplication.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create() => new ApplicationDbContext();
+
+        public DbSet<JoggingRecord> JoggingRecords { get; set; }
+    }
+}
---
+++ DreamLifeWebApplication/Models/JoggingRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DreamLifeWebApplication.Models
+{
+    public class JoggingRecord
+    {
+        public int Id { get; set; }
+
+        public float Distance { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt
+        {
+            get; set;
+        }
+    }
+}
