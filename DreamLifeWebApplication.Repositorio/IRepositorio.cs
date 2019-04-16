adf247d28158319ca50519ae90768dcdf3940334
michaelncarvalho <45688204+michaelncarvalho@users.noreply.github.com>
2019-04-16 10:29:34 -0300

    Create IRepositorio.cs
    Criação interface do Repositorio

---
+++ DreamLifeWebApplication.Repositorio/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamLifeWebApplication.Repositorio
+{
+    public interface IRepositorio<TDominio>
+    {
+        TDominio SelecionarPorId();
+        void Inserir (TDominio entidade);
+        void Atualizar(TDominio entidade);
+        void Excluir(TDominio entity);
+    }
+}
