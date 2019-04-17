30e2232de049e27879ee0fc6dfdcfb3b8261206e
michaelncarvalho <45688204+michaelncarvalho@users.noreply.github.com>
2019-04-17 08:41:11 -0300

    Create CidadeRepositorio.cs
    Criação do Repositorio Cidade

---
+++ DreamLifeWebApplication.Repositorio/CidadeRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using DreamLifeWebApplication.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamLifeWebApplication.Repositorio
+{
+    public class CidadeRepositorio : IRepositorio<Cidade>
+    {
+        public void Atualizar(Cidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Cidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(Cidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cidade SelecionarPorId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+}
