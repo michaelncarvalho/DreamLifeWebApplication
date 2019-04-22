c41008a60d9611c8d638d19db8206efc3fe648cb
michaelncarvalho <45688204+michaelncarvalho@users.noreply.github.com>
2019-04-22 15:40:59 -0300

    Post method for hotel

--- DreamLifeWeb_api/Controllers/HotelsController.cs
+++ DreamLifeWeb_api/Controllers/HotelsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DreamLifeWebApplication.Dominio;
+using DreamLifeWebApplication.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,47 @@ namespace DreamLifeWeb_api.Controllers
 {
     public class HotelsController : ApiController
     {
+        private IRepositorio<Hotel> hotelRepositorio = new HotelRepositorio();
+        //GET: trips/Hotels
+        [HttpGet]
+        public List<Hotel> Get()
+        {
+            return hotelRepositorio.SelecionarTodos();
+        }
+
+        //GET: trips/Hotels/id
+        [HttpGet]
+        public Hotel Get(int id)
+        {
+            return hotelRepositorio.SelecionarPorId(id);
+        }
+
+        //POST: trips/Hotels  
+        [HttpPost]
+        [ActionName("Hotels")]
+        public HttpResponseMessage PostHotels(Hotel hotel)
+        {
+
+            if (ModelState.IsValid && hotel != null)
+            {
+                IRepositorio<Cidade> repositorioCidade = new CidadeRepositorio();
+                Cidade cidade = repositorioCidade.SelecionarPorId(hotel.CidadeId);
+                hotelRepositorio.Inserir(new Hotel()
+                {
+                    Nome = hotel.Nome,
+                    Preco = hotel.Preco,
+                    Classificacao = hotel.Classificacao,
+                    Descricao = hotel.Descricao,
+                    CidadeId = hotel.CidadeId,
+                    Cidade = cidade                    
+                });
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
     }
 }
